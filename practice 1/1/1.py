a649750ba319f0252e953eda6eade1626f1fabdf
BapeHuKcJloByLLlKepoM <148371058+BapeHuKcJloByLLlKepoM@users.noreply.github.com>
2023-11-10 01:48:31 +0500

    Update 1.py

--- practice 1/1/1.py
+++ practice 1/1/1.py
@@ -1,1 +1,16 @@
-#qu'est-ce que c'est 何
+import re
+with open('text_1_var_23', 'r') as file:
+    replacement = re.compile("[^a-zA-Z\d]")
+    text = replacement.sub(" ", str(file.read())).replace("  "," ").strip().split(" ")
+    word_statistics = {}
+
+    for word in text:
+        if word in word_statistics:
+            word_statistics[word] += 1
+        else:
+            word_statistics[word] = 1
+    word_statistics = dict(sorted(word_statistics.items(), reverse=True, key=lambda item: item[1]))
+
+with open('text_1_output', 'w') as output:
+    for word, freq in word_statistics.items():
+       output.write(f'{word}:{freq}\n')
