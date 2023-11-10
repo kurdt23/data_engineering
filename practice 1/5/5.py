a09414ecb7bc905b29e4d9d25613fd3c5e1ed281
BapeHuKcJloByLLlKepoM <148371058+BapeHuKcJloByLLlKepoM@users.noreply.github.com>
2023-11-10 13:14:34 +0500

    Add files via upload

---
+++ practice 1/5/5.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import csv
+
+items = list()
+
+with open('text_5_var_23', encoding='utf-8') as file:
+    lines = file.readlines()
+    html = ''
+    for line in lines:
+        html += line
+
+    soup = BeautifulSoup(html, 'html.parser')
+    rows = soup.find_all('tr')
+    rows = rows[1:]
+    for row in rows:
+        cells = row.find_all('td')
+        item = {'company': cells[0].text,
+            'contact': cells[1].text,
+            'country': cells[2].text,
+            'price': cells[3].text,
+            'item': cells[4].text}
+        items.append(item)
+
+with open('text_5_output.csv', 'w', encoding='utf-8', newline='') as output_file:
+    writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    for item in items:
+        writer.writerow(item.values())
