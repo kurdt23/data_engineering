2c8c1def94c52c1f65c3f3e64530b831759b35cc
BapeHuKcJloByLLlKepoM <148371058+BapeHuKcJloByLLlKepoM@users.noreply.github.com>
2023-12-28 22:01:12 +0500

    Update 4.py

--- practice 3/4/4.py
+++ practice 3/4/4.py
@@ -1,74 +1,75 @@
-import xml.etree.ElementTree as ET
+import collections
 import json
-from zipfile import ZipFile
-from statistics import mean, stdev
+import os
+import zipfile
 
-all_data = []
-freq = {}
+import pandas as pd
+from bs4 import BeautifulSoup
 
-with ZipFile("zip_var_23.zip", "r") as zip_ref:
-    file_list = zip_ref.namelist()
+with zipfile.ZipFile('zip_var_23.zip', 'r') as zip_ref:
+	zip_ref.extractall()
+items = []
+for filename in os.listdir():
+	if filename.endswith(".xml"):
+		with open(filename, "r", encoding="utf-8") as file:
+			text = ""
+			for row in file.readlines():
+				text += row
+			root = BeautifulSoup(text, 'xml')
 
-    for filename in file_list:
-        with zip_ref.open(filename) as file:
-            html_content = file.read()
 
-        root = ET.fromstring(html_content)
+			for clothing in root.find_all("clothing"):
+				item = dict()
+				for el in clothing.contents:
+					if el.name is None:
+						continue
+					elif el.name == "price" or el.name == "reviews":
+						item[el.name] = int(el.get_text().strip())
+					elif el.name == "price" or el.name == "rating":
+						item[el.name] = float(el.get_text().strip())
+					elif el.name == "new":
+						item[el.name] = el.get_text().strip() == "+"
+					elif el.name == "exclusive" or el.name == "sporty":
+						item[el.name] = el.get_text().strip() == "yes"
+					else:
+						item[el.name] = el.get_text().strip()
 
-        clothing_data = []
 
-        for clothing_elem in root.findall(".//clothing"):
-            clothing_id = clothing_elem.findtext("id")
-            name = clothing_elem.findtext("name")
-            category = clothing_elem.findtext("category")
-            size = clothing_elem.findtext("size")
-            color = clothing_elem.findtext("color")
-            material = clothing_elem.findtext("material")
-            price_str = clothing_elem.findtext("price")
-            rating_str = clothing_elem.findtext("rating")
-            reviews_str = clothing_elem.findtext("reviews")
-            sporty = clothing_elem.findtext("sporty")
+				items.append(item)
 
-            clothing_id = clothing_id.strip() if clothing_id is not None else None
-            name = name.strip() if name is not None else None
-            category = category.strip() if category is not None else None
-            size = size.strip() if size is not None else None
-            color = color.strip() if color is not None else None
-            material = material.strip() if material is not None else None
-            price = int(price_str) if price_str is not None else None
-            rating = float(rating_str) if rating_str is not None else None
-            reviews = int(reviews_str) if reviews_str is not None else None
-            sporty = sporty.strip() if sporty is not None else None
+items = sorted(items, key=lambda x: x['category'])
 
-            if material:
-                freq[material] = freq.get(material, 0) + 1
+with open("sorted1.json", "w", encoding="utf-8") as f:
+	f.write(json.dumps(items, indent=2, ensure_ascii=False))
 
-            data = {"id": clothing_id, "name": name, "category": category, "size": size, "color": color,
-                "material": material, "price": price, "rating": rating, "reviews": reviews, "sporty": sporty,}
+filtered_items = []
+for color in items:
+	if color["rating"] > 4.95:
+		filtered_items.append(color)
 
-            all_data.append(data)
+result = []
 
-sorted_f = sorted(all_data, key=lambda x: x["category"])
+df = pd.DataFrame(items)
+pd.set_option('display.float_format', '{:.1f}'.format)
 
-with open("sorted.json", "w", encoding="utf-8") as f:
-    f.write(json.dumps(sorted_f, indent=2, ensure_ascii=False))
+stats = df['reviews'].agg(['sum', 'min', 'max', 'mean', 'std']).to_dict()
+result.append(stats)
 
-filtered_f = list(filter(lambda x: x["rating"] > 4.95, sorted_f))
+result2 = []
 
-with open("filtered.json", "w", encoding="utf-8") as f:
-    f.write(json.dumps(filtered_f, indent=2, ensure_ascii=False))
+material = [item['material'] for item in items]
+f1 = collections.Counter(material)
+result2.append(f1)
 
-values = [item.get("reviews", 0) for item in all_data]
-f_sum = sum(values)
-f_min = min(values)
-f_max = max(values)
-f_avg = mean(values)
-f_stdev = stdev(values)
+with open("filtered1.json", "w", encoding="utf-8") as f:
+	f.write(json.dumps(filtered_items, indent=4, ensure_ascii=False))
 
-sats = {"sum": f_sum, "min": f_min, "max": f_max, "avg": f_avg, "stdev": f_stdev}
+with open("stats1.json", "w", encoding="utf-8") as f:
+	f.write(json.dumps(result, indent=4,  ensure_ascii=False))
 
-with open("sats.json", "w", encoding="utf-8") as f:
-    f.write(json.dumps(sats, indent=2, ensure_ascii=False))
+with open("freq1.json", "w", encoding="utf-8") as f:
+	f.write(json.dumps(result2, indent=4,  ensure_ascii=False))
 
-with open("freq.json", "w", encoding="utf-8") as f:
-    f.write(json.dumps(freq, indent=2, ensure_ascii=False))
+for filename in os.listdir():
+	if filename.endswith(f'.xml'):
+		os.remove(filename)
